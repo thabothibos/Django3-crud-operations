1784eeeda6242797420a0755d571dd1dc5c4c049
Thabo Ndlovu <thabothibosindlovu@gmail.com>
2021-01-04 00:52:32 +0200

    project initialization and django crispy_forms implemented

---
+++ employee_register/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from . models import Employee
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model =  Employee
+        fields = ('fullname', 'mobile', 'emp_code', 'position')
---
+++ employee_register/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .forms import EmployeeForm
+
+# Create your views here.
+def employee_list(request):
+    return render(request, 'employee_register/employee_list.html')
+
+def employee_form(request):
+    form = EmployeeForm()
+    context = {'form':form}
+    return render(request, 'employee_register/employee_form.html', context)
+
+
+def employee_delete(request):
+    return 
