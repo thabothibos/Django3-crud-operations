28e28df19eb35b33e5b70e9d7f872d9532ffb0bb
Thabo Ndlovu <thabothibosindlovu@gmail.com>
2021-01-05 23:15:34 +0200

    final CRUD operations implementeded

--- employee_register/models.py
+++ employee_register/models.py
@@ -1,12 +1,16 @@
 from django.db import models
-from django.core.validators import RegexValidator
-
 # Create your models here.
 class Position(models.Model):
     title = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.title
+
 class Employee(models.Model):
     fullname = models.CharField(max_length=100)
     emp_code = models.CharField(max_length=3)
-    mobile = models.CharField(validators=[RegexValidator(regex=r'^\1?\d{9,10}$')], max_length=10)
+    mobile = models.CharField(max_length=10)
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.fullname
